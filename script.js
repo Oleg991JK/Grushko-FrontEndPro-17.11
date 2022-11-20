6cae01bf20a6c91a6f611217327abf6e690c1999
Oleg991JK <108993918+Oleg991JK@users.noreply.github.com>
2022-11-20 21:55:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const inputData = window.prompt("Choose operation");
+
+const firstNumber = +window.prompt("Enter first number", 0);
+const secondNumber = +window.prompt("Enter second number", 0);
+const add = firstNumber + secondNumber;
+console.log(add);
+
+
+
+switch(inputData) {
+    case "add": 
+      const addResult = firstNumber + secondNumber;
+        alert(`Result ${firstNumber} + ${secondNumber} = ${addResult} `);
+    break
+
+    case "mult":
+        const multResult = firstNumber * secondNumber;
+        alert(`Result ${firstNumber} * ${secondNumber} = ${multResult}`);
+    break
+
+    case "sub":
+        const subResult = firstNumber - secondNumber;
+        alert(`Result ${firstNumber} - ${secondNumber} = ${subResult}`);
+    break  
+
+    case "div":
+        const divResult = firstNumber / secondNumber;
+        alert(`Result ${firstNumber} / ${secondNumber} = ${divResult}`);
+        break
+    default:
+    alert("return to choose operation");
+
+}
